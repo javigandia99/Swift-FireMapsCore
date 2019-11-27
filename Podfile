58574ec9d297a8834d0ab6cd55b341d1237241eb
javier <javigandia99@gmail.com>
2019-11-27 11:58:43 -0800

    Init pattern MVVC & init CocoaPods
    Init pods of firebase & add GoogleInfo-plist 
    Given permissions to use the map

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Swift_FireMapsCore_JGC' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Swift_FireMapsCore_JGC
+  pod 'Firebase'
+  pod 'FirebaseAuth'
+  pod 'FirebaseFirestore'
+
+end
---
+++ Swift_FireMapsCore_JGC/ViewControllers/Register/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  Swift_FireMapsCore_JGC
+//
+//  Created by Javier Gandia on 27/11/2019.
+//  Copyright © 2019 Javier Gandia. All rights reserved.
+//
+
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
