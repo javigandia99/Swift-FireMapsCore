e7e0844929bfd4d7ed8ebd7d22ab6350bfc9984a
javier <javigandia99@gmail.com>
2019-11-27 11:23:21 -0800

    Start Project

---
+++ Swift_FireMapsCore_JGC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_FireMapsCore_JGC
+//
+//  Created by Javier Gandia on 27/11/2019.
+//  Copyright © 2019 Javier Gandia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
