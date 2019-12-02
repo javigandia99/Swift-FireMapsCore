46cbddda3945427eee3e6174e2fc89251925bdae
javier <javigandia99@gmail.com>
2019-12-02 20:28:53 +0100

    Implement Register and fix login GoogleSignIn
    Still error in Google sign in

--- Swift_FireMapsCore_JGC/ViewControllers/Login/LoginViewController.swift
+++ Swift_FireMapsCore_JGC/ViewControllers/Login/LoginViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, GIDSignInDelegate {
     
     // let db = Firestore.firestore()
     
     @IBOutlet weak var gmailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var googleButton: GIDSignInButton!
     
     @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorLabel.alpha = 0
         
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        // Automatically sign in the user.
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        
+        if error != nil {
+            
+            self.showError(error: error!)
+            
+            return
+        }
+        print("Succesfully logged into Google", user!)
+        guard user.authentication.idToken != nil else { return }
+        guard user.authentication.accessToken != nil else { return }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credential, completion: {(user,error) in
+            if error != nil {
+                // Couldn't sign in
+                self.showError(error: error!)
+            }
+            else {
+                //if everything okey go to MapView
+                self.goToMaps()
+            }})
+        goToMaps()
     }
     
     @IBAction func loginButton(_ sender: Any) {
@@ -33,14 +67,18 @@ class LoginViewController: UIViewController {
             
             if error != nil {
                 // Couldn't sign in
-                self.errorLabel.text = error!.localizedDescription
-                self.errorLabel.alpha = 1
+                self.showError(error: error!)
             }
             else {
-                //if all okey go to MapView
+                //if everything okey go to MapView
                 self.goToMaps()
             }
         }
+    }
+    
+    func showError(error:Error){
+        self.errorLabel.text = error.localizedDescription
+        self.errorLabel.alpha = 1
     }
     
     func goToMaps(){
--- Swift_FireMapsCore_JGC/ViewControllers/Register/RegisterViewController.swift
+++ Swift_FireMapsCore_JGC/ViewControllers/Register/RegisterViewController.swift
@@ -12,18 +12,105 @@ import Firebase
 
 class RegisterViewController: UIViewController {
     
-    let db = Firestore.firestore()
     @IBOutlet weak var gmailTxt: UITextField!
     @IBOutlet weak var nameTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorLabel.alpha = 0
         
     }
     
     func makeRegister(){
         let registerDocument = db.collection("users").document()
         registerDocument.setData(["gmail":"\(String(describing: gmailTxt.text))","name":"\(String(describing: nameTxt.text))","password":"\(String(describing: passwordTxt.text))"])
+    }
+    
+    // Check the fields and validate that the data is correct. If everything is correct, this method returns nil. Otherwise, it returns the error message
+    func validateFields() -> String? {
+        
+        // Check that all fields are filled in
+        if gmailTxt.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            nameTxt.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTxt.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        func isPasswordValid(_ password : String) -> Bool {
+            //Regex for check password: Minimum 8 characters at least 1 Alphabet and 1 Number
+            let passwordTest = NSPredicate(format: "SELF MATCHES %@","^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+            return passwordTest.evaluate(with: password)
+        }
+        
+        // Check if the password is secure
+        let cleanedPassword = passwordTxt.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Please make sure your password is at least 8 characters at least 1 Alphabet and 1 Number."
+        }
+        
+        return nil
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        // Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            // There's something wrong with the fields, show error message
+            self.showError(error: error!)
+        }
+        else {
+            
+            // Create cleaned versions of the data
+            let gmail = gmailTxt.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let name = nameTxt.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTxt.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: gmail, password: password) { (result, err) in
+                
+                // Check for errors
+                if error != nil {
+                    
+                    // There was an error creating the user
+                    self.showError(error: error as! Error as! String )
+                }
+                else {
+                    
+                    // User was created successfully, now store the gmsil and name
+                    self.db.collection("users").addDocument(data: ["gmail":gmail, "name":name, "uid": result!.user.uid ]) { (error) in
+                        
+                        if error != nil {
+                            self.showError(error: error! as! String)
+                        }
+                    }
+                    
+                    // Transition to the Maps screen
+                    self.goToMaps()
+                }
+            }
+        }
+    }
+    
+    func showError(error:String){
+        self.errorLabel.text = error
+        self.errorLabel.alpha = 1
+    }
+    
+    func goToMaps(){
+        
+        let mapsView = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.mapsViewController) as? MapsViewController
+        
+        self.view.window?.rootViewController = mapsView
+        self.view.window?.makeKeyAndVisible()
     }
 }
