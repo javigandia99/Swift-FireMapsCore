8df990b3336bede8d8305ca698aab567da7ce965
javier <javigandia99@gmail.com>
2019-12-04 22:12:44 +0100

    Implement Search by address in MapView
    Add button to centerUserLocation
    Add button to Change type of the map: standard & satellite
    Changes in design StoryBoard

--- Swift_FireMapsCore_JGC/ViewControllers/Maps/MapsViewController.swift
+++ Swift_FireMapsCore_JGC/ViewControllers/Maps/MapsViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapsViewController: UIViewController {
+class MapsViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var searchText: UITextField!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var myLocation: UIButton!
     
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
@@ -24,20 +24,18 @@ class MapsViewController: UIViewController {
     let geoCoder = CLGeocoder()
     var directionsArray: [MKDirections] = []
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         goButton.layer.cornerRadius = goButton.frame.size.height/2
+        myLocation.layer.cornerRadius = myLocation.frame.size.height/2
         mapView.delegate = self
         checkLocationServices()
     }
-    
     
     func setupLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
-    
     
     func centerViewOnUserLocation() {
         if let location = locationManager.location?.coordinate {
@@ -82,6 +80,10 @@ class MapsViewController: UIViewController {
         centerViewOnUserLocation()
         locationManager.startUpdatingLocation()
         previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    @IBAction func goToMyLocation(_ sender: UIButton) {
+        centerViewOnUserLocation()
     }
     
     func getCenterLocation(for mapView: MKMapView) -> CLLocation {
@@ -130,6 +132,84 @@ class MapsViewController: UIViewController {
         return request
     }
     
+    @IBAction func changeMap(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            
+            mapView.mapType = .standard
+            
+        }else{
+            
+            mapView.mapType = .satellite
+            
+        }
+    }
+    
+    @IBAction func searchButton(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        //Ignoring user
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //Activity Indicator
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        self.view.addSubview(activityIndicator)
+        
+        //Hide search bar
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        
+        //Create the search request
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        
+        activeSearch.start { (response, error) in
+            
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            
+            if error != nil{
+                
+                print("ERROR searching Address")
+                
+            }else{
+                //Remove annotations
+                let annotations = self.mapView.annotations
+                self.mapView.removeAnnotations(annotations)
+                
+                //Getting data
+                let latitude = response?.boundingRegion.center.latitude
+                let longitude = response?.boundingRegion.center.longitude
+                
+                //Create annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = searchBar.text
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                self.mapView.addAnnotation(annotation)
+                
+                //Zooming in on annotation
+                let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+                let region = MKCoordinateRegion(center: coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
+                //Show routes to go to address
+                self.getDirections()
+            }
+            
+        }
+    }
+    
     func resetMapView(withNew directions: MKDirections) {
         mapView.removeOverlays(mapView.overlays)
         directionsArray.append(directions)
@@ -137,13 +217,13 @@ class MapsViewController: UIViewController {
     }
     
     func showAlert(title:String,message:String){
-    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "Okey", style: .default, handler: nil))
-    alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-    
-    self.present(alert, animated: true)
-}
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Okey", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
 }
 
 extension MapsViewController: CLLocationManagerDelegate {
@@ -169,12 +249,12 @@ extension MapsViewController: MKMapViewDelegate {
             guard let self = self else { return }
             
             if let _ = error {
-                //TODO: Show alert informing the user
+                self.showAlert(title: "ERROR", message: error!.localizedDescription)
                 return
             }
             
             guard let placemark = placemarks?.first else {
-               self.addressLabel.text = "ERROR"
+                self.addressLabel.text = "ERROR"
                 return
             }
             
