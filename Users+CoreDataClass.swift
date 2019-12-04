bffc45dfeb507f8c93fc2004331b3b299af53ca3
javier <javigandia99@gmail.com>
2019-12-04 17:54:29 +0100

    Add CoreData: Properties and Class Users

---
+++ Users+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Users+CoreDataClass.swift
+//  
+//
+//  Created by Javier Gandia on 04/12/2019.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Users)
+public class Users: NSManagedObject {
+
+}
---
+++ Users+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Users+CoreDataProperties.swift
+//  
+//
+//  Created by Javier Gandia on 04/12/2019.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Users {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Users> {
+        return NSFetchRequest<Users>(entityName: "Users")
+    }
+
+    @NSManaged public var gmail: String?
+    @NSManaged public var name: String?
+
+}
